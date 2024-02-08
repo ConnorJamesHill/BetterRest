f27f8f1ac99fb6fec5f77997636e585cb1e756ab
Meruem Moniker <jamesconnorhill@gmail.com>
2024-02-07 20:22:14 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Connor Hill on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
